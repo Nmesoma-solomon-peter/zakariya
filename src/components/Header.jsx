403cd34e173be50d42bd25bd802584844d8f0e39
nmesoma <nmesomasolo2809@gmail.com>
2023-01-08 09:58:52 +0100

    exit header and add animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,44 @@
 import React from 'react'
+import { motion } from "framer-motion"
 import './Header.css'
+import dribble from '../assets/dribble.png'
+import behance from '../assets/behance.png'
+import linkedin from '../assets/linkedin.png'
 
 function Header() {
     return (
         <div className='header'>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">My Portfolio</a>
+                    <motion.a className="navbar-brand" href="#"
+                        initial={{ x: -200 }}
+                        animate={{ x: 2 }}
+                        transition={{ delay: 1 }}
+                    >My Portfolio</motion.a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <motion.ul className="navbar-nav ms-auto mb-2 mb-lg-0"
+                            initial={{ y: -70 }}
+                            animate={{ y: -2 }}
+                            transition={{ delay: 1 }}
+                        >
                             <li className="nav-item text-decoration-underline">
                                 <a className="nav-link " aria-current="page" href="https://www.behance.net/c5da0bce">Read Case Studies:</a>
                             </li>
                             <li className="nav-item text-decoration-underline linkborder me-2">
-                                <a className="nav-link active" href="https://www.behance.net/c5da0bce">Behance</a>
+                                <a className="nav-link active" href="https://behance.net/c5da0bce"><img src={behance} alt="bahance" className='headericon behance' /></a>
                             </li>
-                            
+
                             <li className="nav-item text-decoration-underline  linkborder me-2">
-                                <a className="nav-link active" href="https://www.linkedin.com/in/zakariya-buhari-929076154/">Zakariya Buhari | LinkedIn</a>
+                                <a className="nav-link active" href="https://linkedin.com/in/zakariya-buhari-929076154/"><img src={linkedin} className='headericon ' /></a>
                             </li>
-                            
+
                             <li className="nav-item">
-                                <a className="nav-link active linkborder" href="https://dribbble.com/saka04">Dribble</a>
+                                <a className="nav-link active linkborder" href="https://dribbble.com/saka04"><img src={dribble} className='headericon ' /></a>
                             </li>
-                        </ul>
+                        </motion.ul>
                     </div>
                 </div>
             </nav>
--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,14 +1,127 @@
 import React from 'react'
+import { useEffect } from "react";
+import { motion} from "framer-motion"
+// import { useInView } from 'react-intersection-observer';
 import './Section2.css'
+// import transitions from '@material-ui/core/styles/transitions';
+// import { Visibility } from '@material-ui/icons'
 // import sec2img from '../assets/sec2img.png'
 // import sec3img from '../assets/sec3img.png'
 // import sec4img from '../assets/sec4img.png'
+// const scrollRef = useRef(null)
+// const row1leftv = {
+//     hidden: {
+//         x: -800,
+//     },
+//     visible: {
+//         x: 2,
+//         transition: { delay: 1, duration: 2 }
+//     }
+// }
+// const row2leftv = {
+//     hidden: {
+//         x: 800,
+//     },
+//     visible: {
+//         x: -2,
+//         transition: { delay: 1, duration: 2 }
+//     }
+
+// }
+// const rowtwov = {
+//     hidden: {
+//         x: 800,
+//     },
+//     visible: {
+//         x: -2,
+//         transition: { delay: 0, duration: 1 }
+//     }
+// }
+// const rowtworv = {
+//     hidden: {
+//         y: -800,
+//     },
+//     visible: {
+//         y: 2,
+//         transition: { delay: 0, duration: 1 }
+//     }
+// }
+const row1left={
+    hidden:{
+        x:100,
+    },
+    visible:{
+        x:2,
+        transition:{delay:1,duration:2}
+    }
+}
+
+const row1right={
+    hidden:{
+        x:600,
+    },
+    visible:{
+        x:-2,
+        transition:{delay:1,duration:2}
+    }
+}
+const row2left={
+    hidden:{
+        x:800,
+    },
+    visible:{
+        x:-2,
+        transition:{delay:1,duration:1}
+    }
+}
+const row2right={
+    hidden:{
+        x:200,
+    },
+    visible:{
+        x:2,
+        transition:{delay:1,duration:1}
+    }
+}
+const row3left={
+    hidden:{
+        width:300
+    },
+    visible:{
+      width:600,
+        transition:{delay:1,duration:1}
+    }
+}
 
 function Section2() {
+    // const { ref, inView } = useInView({
+    //     // threshold: 0.2
+    // });
+    // const animation = useAnimation();
+    // useEffect(() => {
+    //     console.log("useeffect hook renders = ", inView);
+    //     if(inView) {
+    //         animation.start({
+    //             x: 5,
+    //             transition: { type: 'spring', duration: 1 }
+    //         });
+    //         if(!inView) {
+    //             animation.start({
+    //                 x: -800,
+    //             })
+    //         }
+    //     }
+    // }, [inView]);
+
     return (
         <div className='container-fluid section2'>
             <div className='row secwithborder'>
-                <div className='col col-md-6 '>
+                <motion.div className='col col-md-6 '
+                    variants={row1left}
+                    initial="hidden"
+                    whileInView="visible"
+                    // viewport={{ once: true }}
+                >
                     <h3 className='fw-bold sech3 mb-3'>DesignBlocs </h3>
                     <p className='mb-5'>Design Blocs is a design language created to see to the needs of Product designers looking to design user centric products. </p>
 
@@ -20,21 +133,32 @@ function Section2() {
                     </div>
                     <div className='d-flex secflex'>
                         <p>Visit Website</p>
-                        <p><a href='https://t.co/fFTTbPWOcZ'>https://t.co/fFTTbPWOcZ</a></p>
+                        <p><a href='https://t.co/fFTTbPWOcZ'>https://designblocs.com/</a></p>
                     </div>
                     <div className='d-flex secflex'>
                         <p>Deliverables</p>
                         <p>UI Design, UX Design</p>
                     </div>
-                </div>
-                <div className='col col-md-6'>
+                </motion.div>
+                <motion.div className='col col-md-6'
+                    variants={row1right}
+                    initial="hidden"
+                    whileInView="visible"
+                    // viewport={{ once: true }}
+                >
                     <img src='/sec2img.png' alt='sec2img' className='sec2img' />
-                </div>
+                </motion.div>
             </div>
 
             {/* second row */}
-            <div className='row secwithborder secrow'>
-                <div className='col col-md-6 '>
+            <div className='row secwithborder secrow '>
+                <motion.div className='col col-md-6 '
+                    variants={row2left}
+                    initial="hidden"
+                    whileInView="visible"
+                    // viewport={{ once: true }}
+                    // animate={animation}
+                >
                     <h3 className='fw-bold sech3 mb-3'>Kwikinvoice</h3>
                     <p className='mb-5'>KwikInvoice is a platform where online business persons, small, medium and large enterprise can generate Dynamic Invoices in less than 2 minutes, For Free. </p>
 
@@ -46,20 +170,31 @@ function Section2() {
                     </div>
                     <div className='d-flex secflex'>
                         <p>Visit Website</p>
-                        <p><a href='https://t.co/ZCEylPIlCS'>https://t.co/ZCEylPIlCS</a></p>
+                        <p><a href='https://t.co/ZCEylPIlCS'>https://kwikinvoice.com/</a></p>
                     </div>
                     <div className='d-flex secflex'>
                         <p>Deliverables</p>
                         <p>UI Design, UX Design</p>
                     </div>
-                </div>
-                <div className='col col-md-6'>
+                </motion.div>
+                <motion.div className='col col-md-6'
+                    variants={row2right}
+                    initial="hidden"
+                    whileInView="visible"
+                    // viewport={{ once: true }}
+                    // animate={animation}
+                >
                     <img src='/sec3img.png' alt='sec3img' className='sec2img' />
-                </div>
+                </motion.div>
             </div>
             {/* section3 */}
             <div className='row secrow'>
-                <div className='col col-md-6 '>
+                <motion.div className='col col-md-6 '
+                    variants={row3left}
+                    initial="hidden"
+                    whileInView="visible"
+                    // viewport={{ once: true }}
+                >
                     <h3 className='fw-bold sech3 mb-3'>ClientDorm</h3>
                     <p className='mb-5'>A free CRM app built around your business just how you want it. It is totally free to use CRM app built to accommodate your business needs and tailored down to how you would want it to be. </p>
 
@@ -71,16 +206,20 @@ function Section2() {
                     </div>
                     <div className='d-flex secflex '>
                         <p>Visit Website</p>
-                        <p><a href='https://t.co/s7BKuVWFGQ'>https://t.co/s7BKuVWFGQ</a></p>
+                        <p><a href='https://t.co/s7BKuVWFGQ'>https://clientdorm.com/</a></p>
                     </div>
                     <div className='d-flex secflex'>
                         <p>Deliverables</p>
                         <p>UI Design, UX Design</p>
                     </div>
-                </div>
-                <div className='col col-md-6'>
+                </motion.div>
+                <motion.div className='col col-md-6'
+                    // variants={ rowtwov }
+                    // initial="hidden"
+                    // whileInView="visible"
+                >
                     <img src='/sec4img.png' alt='sec4img' className='sec2img' />
-                </div>
+                </motion.div>
             </div>
         </div>
     )
